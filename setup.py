6c94b667ecbe07fda71d32d62a26378c43a85294
Zhong Zhenyu <nczzy1997@gmail.com>
2020-10-24 11:23:26 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='bagel-tensorflow',
+    version='1.0.0',
+    author='AlumiK',
+    author_email='nczzy1997@gmail.com',
+    description='TensorFlow implementation of Bagel',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/AlumiK/bagel-tensorflow',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.8',
+)
