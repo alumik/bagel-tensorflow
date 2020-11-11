3b77fcfbd9f6ea376a704cfa5edfc4b4a5c293ec
Zhong Zhenyu <nczzy1997@gmail.com>
2020-11-11 15:09:19 +0800

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='bagel-tensorflow',
-    version='1.4.0',
+    version='1.4.3',
     author='AlumiK',
     author_email='nczzy1997@gmail.com',
     license='MIT',
